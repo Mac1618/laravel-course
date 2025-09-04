604ecafe6a52d88f1c9d18f2c0ac31ea1d918340
Mark Bongulto <mac46099@gmail.com>
2025-09-04 18:01:40 +0800

    fea: learned eloquent relationship about belongsToMany

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Support\Arr;
 
 class Job extends Model {
@@ -21,5 +22,9 @@ class Job extends Model {
   // job = mark, employer = intellect
   public function employer() {
     return $this->belongsTo(Employer::class);
+  }
+
+  public function tags () {
+    return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
   }
 }
